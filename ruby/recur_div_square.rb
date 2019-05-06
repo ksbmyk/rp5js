ca22c25ebff555712dde72405481ab0148298abf
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-06 23:30:51 +0900

    Add ruby program for recur div square

---
+++ ruby/recur_div_square.rb
@@ -0,0 +1,13 @@
+$numA = 10;
+$numB = 6;
+$ratio = numB / numA;
+$thr = 160;  //しきい値
+def setup(){
+  P5.createCanvas(500, 500)
+  P5.colorMode(HSB, 1)
+  divSquare(0, 0, width)
+}
+
+ //正方形の分割
+def divSquare(xPos, yPos, wd)
+end

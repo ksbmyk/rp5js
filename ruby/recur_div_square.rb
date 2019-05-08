75b018759ad1cbbcf2d41ec059821c892d0c2bd3
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-08 23:51:27 +0900

    Fix typo

--- ruby/recur_div_square.rb
+++ ruby/recur_div_square.rb
@@ -1,14 +1,18 @@
-
-def setup(){
-  @numA = 10;
-  @numB = 6;
-  @ratio = @numB / @numA;
-  @thr = 160;  //しきい値
+def setup
+  @numA = 10
+  @numB = 6
+  @ratio = @numB / @numA
+  @thr = 160  #しきい値
   P5.createCanvas(500, 500)
-  P5.colorMode(HSB, 1)
-  divSquare(0, 0, width)
-}
+  P5.colorMode(P5.HSB, 1)
+  #divSquare(0, 0, width)
+  divSquare(0, 0, 100)
+end
 
- //正方形の分割
+#正方形の分割
 def divSquare(xPos, yPos, wd)
+
+end
+
+def draw
 end

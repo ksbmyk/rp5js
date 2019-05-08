efce8b473e3a2765f2cb3512fca9d1271ed218aa
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-08 23:40:34 +0900

    Use instance variable

--- ruby/recur_div_square.rb
+++ ruby/recur_div_square.rb
@@ -1,8 +1,9 @@
-$numA = 10;
-$numB = 6;
-$ratio = numB / numA;
-$thr = 160;  //しきい値
+
 def setup(){
+  @numA = 10;
+  @numB = 6;
+  @ratio = @numB / @numA;
+  @thr = 160;  //しきい値
   P5.createCanvas(500, 500)
   P5.colorMode(HSB, 1)
   divSquare(0, 0, width)

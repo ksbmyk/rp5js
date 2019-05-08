b50c88999792379bce6b2f4884cebfd498f32816
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-08 23:28:43 +0900

    Use instance variable

--- ruby/rect.rb
+++ ruby/rect.rb
@@ -1,40 +1,38 @@
 NUMA = 10
 NUMB = 6
 SCALAR = 50
-$numA = NUMA * SCALAR
-$numB = NUMB * SCALAR
-
-$wd = $numB
-$xPos = 0
-$yPos = 0
-$itr = 0
 
 def setup
+  @numA = NUMA * SCALAR
+  @numB = NUMB * SCALAR
+  @wd = @numB
+  @xPos = 0
+  @yPos = 0
+  @itr = 0
   P5.createCanvas(600, 600)
   # HSB, 色相の範囲, 彩度の範囲, 明度の範囲
   P5.colorMode(P5.HSB, 1, 100, 100)
 end
 
-
 def draw
-  while ($wd > 0) do
-    $itr += 1
-    if ($itr % 2 == 1)
-      while ($xPos + $wd <= $numA) do
+  while (@wd > 0) do
+    @itr += 1
+    if (@itr % 2 == 1)
+      while (@xPos + @wd <= @numA) do
         P5.stroke(0,0,60)
         P5.fill(color_code)
-        P5.rect($xPos, $yPos, $wd, $wd)
-        $xPos += $wd
+        P5.rect(@xPos, @yPos, @wd, @wd)
+        @xPos += @wd
       end
-      $wd = $numA - $xPos
+      @wd = @numA - @xPos
     else
-      while ($yPos + $wd <= $numB) do
+      while (@yPos + @wd <= @numB) do
         P5.stroke(0,0,60)
         P5.fill(color_code)
-        P5.rect($xPos, $yPos, $wd, $wd)
-        $yPos += $wd
+        P5.rect(@xPos, @yPos, @wd, @wd)
+        @yPos += @wd
       end
-      $wd = $numB - $yPos
+      @wd = @numB - @yPos
     end
   end
 end

32c75ef01200f49c51f0a8b1f0d75d729d7c2618
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-08 23:37:20 +0900

    Use instance variable

--- math_gene.rb
+++ math_gene.rb
@@ -1,21 +1,21 @@
 NUM = 100
 MOD = 20
-$gen = 0
-$state = Array.new(NUM).map{Array.new(NUM, 0)}
 
 def setup
+  @gen = 0
+  @state = Array.new(NUM).map{Array.new(NUM, 0)}
   P5.createCanvas(500, 500)
   P5.colorMode(P5.HSB, 1)
-  $state[0][0] = 1
+  @state[0][0] = 1
 end
 
 def draw
   drawCell
   updateState
-  if $gen == 187
+  if @gen == 187
     P5.noLoop()
   end
-  $gen += 1
+  @gen += 1
 end
 
 def drawCell
@@ -26,7 +26,7 @@ def drawCell
     x = 0.0
     NUM.times do |j|
       P5.noStroke()
-      col = ($state[i][j] * 1.0) / MOD
+      col = (@state[i][j] * 1.0) / MOD
       P5.fill(col, col, 1)
       P5.rect(x, y, scalar, scalar)
       x += scalar
@@ -43,14 +43,14 @@ def updateState
       nextState[i][j] = transition(i, j)
     end
   end
-  $state = nextState
+  @state = nextState
 end
 
 def transition(i, j)
-  nextC = $state[(i - 1 + NUM) % NUM][j]
-    + $state[i][(j - 1 + NUM) % NUM]
-    + $state[i][j]
-    + $state[i][(j + 1) % NUM]
-    + $state[(i + 1) % NUM][j]
+  nextC = @state[(i - 1 + NUM) % NUM][j]
+    + @state[i][(j - 1 + NUM) % NUM]
+    + @state[i][j]
+    + @state[i][(j + 1) % NUM]
+    + @state[(i + 1) % NUM][j]
   nextC % MOD
 end

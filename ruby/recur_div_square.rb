91de0e23ead3fb8fda923dd789e216fbc73db032
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-10 21:11:34 +0900

    Refactor divSquare

--- ruby/recur_div_square.rb
+++ ruby/recur_div_square.rb
@@ -5,17 +5,34 @@ def setup
   @thr = 160  #しきい値
   P5.createCanvas(500, 500)
   P5.colorMode(P5.HSB, 1)
-  #divSquare(0, 0, width)
+  #divSquare(0, 0, width) #TODO:widthの初期値
   divSquare(0, 0, 100)
 end
 
 #正方形の分割
 def divSquare(xPos, yPos, wd)
-  @itr = 0
-  @xEndPos = wd + xPos
-  @yEndPos = wd + yPos
+  itr = 0
+  xEndPos = wd + xPos
+  yEndPos = wd + yPos
   P5.fill(P5.color(P5.random(1), 1, 1))
   P5.rect(xPos, yPos, wd, wd)
+  while wd > @thr do
+    itr += 1
+    if (itr % 2 == 1)
+      while (xPos + wd * @ratio < xEndPos + 0.1) do
+        divRect(xPos, yPos, wd * @ratio)
+      end
+      xPos += wd * @ratio
+    else
+      divRect(xPos, yPos, wd)
+      yPos += wd / @ratio
+    end
+    wd = yEndPos - yPos
+  end
+end
+
+#長方形を分割する関数
+def divRect(xPos, yPos, wd)
 end
 
 def draw

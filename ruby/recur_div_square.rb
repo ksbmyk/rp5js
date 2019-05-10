813d7b9d481524d26527b51b66d8d6fac3733c5a
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-10 21:12:07 +0900

    Add div rect method

--- ruby/recur_div_square.rb
+++ ruby/recur_div_square.rb
@@ -33,6 +33,27 @@ end
 
 #長方形を分割する関数
 def divRect(xPos, yPos, wd)
+  itr = 0
+  xEndPos = wd + xPos
+  yEndPos = wd + yPos
+  P5.fill(color(P5.random(1), 1, 1))
+  P5.rect(xPos, yPos, wd, wd)
+  while (wd > thr) do
+    itr += 1
+    if itr % 2 == 0
+      while (xPos + wd < xEndPos + 0.1) do
+        divSquare(xPos, yPos, wd)
+        xPos += wd
+      end
+      wd = xEndPos - xPos
+    else
+      while (yPos + wd < yEndPos + 0.1) do
+        divSquare(xPos, yPos, wd)
+        yPos += wd
+      end
+      wd = yEndPos - yPos
+    end
+  end
 end
 
 def draw

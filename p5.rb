6db3fea52ed3a990e9cd15b90750bda4138ffdbd
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-11 20:35:25 +0900

    Wrap p5js method

--- p5.rb
+++ p5.rb
@@ -1,12 +1,58 @@
 module P5
   def self.method_missing(name, *args)
+    #puts name
     %x{
       obj = window[name];
+      // 関数かどうか
       if (typeof(obj) == 'function') {
+        // ラップしきれていない関数の呼び出し
         return window[name].apply(window, args);
       } else {
+        // グローバル変数。HBSとか
         return window[name];
       }
+    }
+  end
+
+  def self.createCanvas(*args)
+    %x{
+      return window['createCanvas'].apply(window, args);
+    }
+  end
+
+  def self.colorMode(*args)
+    %x{
+      return window['colorMode'].apply(window, args);
+    }
+  end
+
+  def self.stroke(*args)
+    %x{
+      return window['stroke'].apply(window, args);
+    }
+  end
+
+  def self.fill(*args)
+    %x{
+      return window['fill'].apply(window, args);
+    }
+  end
+
+  def self.rect(*args)
+    %x{
+      return window['rect'].apply(window, args);
+    }
+  end
+
+  def self.random(*args)
+    %x{
+      return window['random'].apply(window, args);
+    }
+  end
+
+  def self.color(*args)
+    %x{
+      return window['color'].apply(window, args);
     }
   end
 

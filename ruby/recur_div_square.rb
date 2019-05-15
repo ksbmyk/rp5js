76810c182e6d30352a74be19017099648aa597e1
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-15 23:26:43 +0900

    Use constant for Vertical and side

--- ruby/recur_div_square.rb
+++ ruby/recur_div_square.rb
@@ -1,8 +1,9 @@
 THR = 160 #しきい値
+NUMA = 10
+NUMB = 6
+
 def setup
-  @numA = 10
-  @numB = 6
-  @ratio = @numB / @numA
+  @ratio = NUMB / NUMA
   P5.createCanvas(500, 500)
   P5.colorMode(P5.HSB, 1)
   divSquare(0, 0, P5.width-1)

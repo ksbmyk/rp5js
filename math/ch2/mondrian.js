c7411a3cb8fbcb0d6c3dc4b21f7cdd40a80d8757
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-16 22:58:12 +0900

    Create mondrian base

---
+++ math/ch2/mondrian.js
@@ -0,0 +1,17 @@
+var ratio = (sqrt(5) + 1) / 2;  //黄金数
+var thr = 80;  //float 分割する大きさに関するしきい値
+var thr2 = 0.5; //確率を決定するしきい値
+function setup(){
+  createCanvas(500, 500);
+  colorMode(HSB, 1);
+  colorRect(0, 0, width, width);
+  divSquare(0, 0, width);
+}
+
+function colorRect(x, y, width, width){
+
+}
+
+function divSquare(x, y, width){
+
+}

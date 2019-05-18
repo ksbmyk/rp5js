43aedfda38df3c672b2e69ff41f57cb20195e0a7
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-18 22:59:53 +0900

    Add colorRect method

--- math/ch2/mondrian.js
+++ math/ch2/mondrian.js
@@ -1,4 +1,4 @@
-var ratio = (sqrt(5) + 1) / 2;  //黄金数
+var ratio = (sqrt(5) + 1) / 2;  //黄金数 //sqrt is not defined
 var thr = 80;  //float 分割する大きさに関するしきい値
 var thr2 = 0.5; //確率を決定するしきい値
 function setup(){
@@ -8,8 +8,25 @@ function setup(){
   divSquare(0, 0, width);
 }
 
-function colorRect(x, y, width, width){
-
+function colorRect(xPos, yPos, wd, ht){
+  let col;
+  var val = random(1);
+  if (val < 0.15){  //15%の確率
+    col = color(0, 1, 1); //赤
+  }else if (val < 0.3){ //15%の確率
+    col = color(2.0 / 3, 1, 1); //青
+  }else if (val < 0.45){  //15%の確率
+    col = color(1.0 / 6, 1, 1); //黄
+  }else if (val < 0.5){ //5%の確率
+    col = color(0, 1, 0); //黒
+  } else if (val < 0.7){  //20%の確率
+    col = color(0, 0, 0.9); //灰
+  } else {  //30%の確率
+    col = color(0, 0, 1); //白
+  }
+  fill(col);
+  strokeWeight(5);  //長方形の枠線の太さ
+  rect(xPos, yPos, wd, ht);
 }
 
 function divSquare(x, y, width){

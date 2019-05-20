48bf434ee3559a56b756199079b607d874138cc7
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-20 18:56:40 +0900

    Add mouseClicked and draw method

--- math/ch2/mondrian.js
+++ math/ch2/mondrian.js
@@ -60,3 +60,12 @@ function divSquare(xPos, yPos, wd){
 function divRect(xPos, yPos, wd){
 
 }
+
+function mouseClicked(){
+  thr = int(random(10, 300));
+  thr2 = random(0,1);
+  println("thr =", thr, "thr2 =", thr2);
+  colorRect(0, 0, width, width);
+  divSquare(0, 0, width);
+}
+function draw(){}

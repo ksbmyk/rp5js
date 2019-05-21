6519daab76026fc5e3fe0b640a1deee0febe7bac
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-21 21:17:35 +0900

    Delete unneed space

--- math/ch2/mondrian.js
+++ math/ch2/mondrian.js
@@ -20,9 +20,9 @@ function colorRect(xPos, yPos, wd, ht){
     col = color(1.0 / 6, 1, 1); //黄
   }else if (val < 0.5){ //5%の確率
     col = color(0, 1, 0); //黒
-  } else if (val < 0.7){  //20%の確率
+  }else if (val < 0.7){  //20%の確率
     col = color(0, 0, 0.9); //灰
-  } else {  //30%の確率
+  }else {  //30%の確率
     col = color(0, 0, 1); //白
   }
   fill(col);

b93bb7016c51b5b2cb6279a6bf82f21cc1051393
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-25 22:03:47 +0900

    Add setup method details

--- ruby/mondrian.rb
+++ ruby/mondrian.rb
@@ -3,6 +3,10 @@ THR = 80  #分割する大きさに関するしきい値 TODO あとで名前考
 THR2 = 0.5 #確率を決定するしきい値
 
 def setup
+  P5.createCanvas(500, 500)
+  P5.colorMode(P5.HSB, 1)
+  colorRect(0, 0, P5.width - 1, P5.width - 1)
+  divSquare(0, 0, P5.width - 1)
 end
 
 def draw

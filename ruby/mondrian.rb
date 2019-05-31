94dad4f1e0c13a3ff7b4d2b3250d9faac9473f18
Miyuki Koshiba <ksbmyk@gmail.com>
2019-05-31 22:21:06 +0900

    Add space to comments

--- ruby/mondrian.rb
+++ ruby/mondrian.rb
@@ -1,8 +1,8 @@
-RATIO = (2.23606797749979 + 1) / 2 #比率 5の平方根に1足して2で割る
+RATIO = (2.23606797749979 + 1) / 2 # 比率 5の平方根に1足して2で割る
 
 def setup
-  @thr = 80  #分割する大きさに関するしきい値 TODO あとで名前考え直す
-  @thr2 = 0.5 #確率を決定するしきい値
+  @thr = 80  # 分割する大きさに関するしきい値 TODO あとで名前考え直す
+  @thr2 = 0.5 # 確率を決定するしきい値
   P5.create_canvas(500, 500)
   P5.color_mode(P5.HSB, 1)
   colorRect(0, 0, P5.width - 1, P5.width - 1)
@@ -21,44 +21,44 @@ end
 
 def colorRect(xPos, yPos, wd, ht)
   case P5.random(1)
-  when 0...0.15 #15%の確率
-    col = P5.color(0, 1, 1) #赤
-  when 0.15...0.3 #15%の確率
-    col = P5.color(2.0 / 3, 1, 1) #青
-  when 0.3...0.45  #15%の確率
-    col = P5.color(1.0 / 6, 1, 1) #黄
-  when 0.45...0.5 #5%の確率
-    col = P5.color(0, 1, 0) #黒
+  when 0...0.15 # 15%の確率
+    col = P5.color(0, 1, 1) # 赤
+  when 0.15...0.3 # 15%の確率
+    col = P5.color(2.0 / 3, 1, 1) # 青
+  when 0.3...0.45  # 15%の確率
+    col = P5.color(1.0 / 6, 1, 1) # 黄
+  when 0.45...0.5 # 5%の確率
+    col = P5.color(0, 1, 0) # 黒
   when 0.5...0.7  #20%の確率
-    col = P5.color(0, 0, 0.9) #灰
-  else   #30%の確率
-    col = P5.color(0, 0, 1) #白
+    col = P5.color(0, 0, 0.9) # 灰
+  else   # 30%の確率
+    col = P5.color(0, 0, 1) # 白
   end
   P5.fill(col)
-  P5.strokeWeight(5)  #長方形の枠線の太さ
+  P5.strokeWeight(5)  # 長方形の枠線の太さ
   P5.rect(xPos, yPos, wd, ht)
 end
 
 def divSquare(xPos, yPos, wd)
   itr = 0
-  xEndPos = wd + xPos  #正方形の横の長さ
-  yEndPos = wd + yPos  #正方形の縦の長さ
-  while wd > @thr do  #正方形の幅がしきい値以上の場合に実行
+  xEndPos = wd + xPos  # 正方形の横の長さ
+  yEndPos = wd + yPos  # 正方形の縦の長さ
+  while wd > @thr do  # 正方形の幅がしきい値以上の場合に実行
     itr += 1
     if itr % 2 == 1
       while (xPos + wd * RATIO < xEndPos + 0.1) do
-        colorRect(xPos, yPos, wd * RATIO, wd)  #長方形を描く
-        if P5.random(1) < @thr2  #thr2の確率で再分割
-          divRect(xPos, yPos, wd * RATIO)  #長方形を分割する関数の呼び出し
+        colorRect(xPos, yPos, wd * RATIO, wd)  # 長方形を描く
+        if P5.random(1) < @thr2  # thr2の確率で再分割
+          divRect(xPos, yPos, wd * RATIO)  # 長方形を分割する関数の呼び出し
         end
         xPos += wd * RATIO
       end
       wd = xEndPos - xPos
     else
       while (yPos + wd / RATIO < yEndPos + 0.1) do
-        colorRect(xPos, yPos, wd, wd / RATIO)  #長方形を描く
-        if P5.random(1) < @thr2  #thr2の確率で再分割
-          divRect(xPos, yPos, wd)  #長方形を分割する関数の呼び出し
+        colorRect(xPos, yPos, wd, wd / RATIO)  # 長方形を描く
+        if P5.random(1) < @thr2  # thr2の確率で再分割
+          divRect(xPos, yPos, wd)  # 長方形を分割する関数の呼び出し
         end
         yPos += wd / RATIO
       end
@@ -69,24 +69,24 @@ end
 
 def divRect(xPos, yPos, wd)
   itr = 0
-  xEndPos = xPos + wd  #長方形の横の長さ
-  yEndPos = yPos + wd / RATIO   #長方形の縦の長さ
-  while (wd > @thr)   #wdがしきい値以上の場合に処理を行う
+  xEndPos = xPos + wd  # 長方形の横の長さ
+  yEndPos = yPos + wd / RATIO   # 長方形の縦の長さ
+  while (wd > @thr)   # wdがしきい値以上の場合に処理を行う
     itr += 1
     if (itr % 2 == 0)
       while (xPos + wd < xEndPos + 0.1) do
-        colorRect(xPos, yPos, wd, wd)  #正方形を描く
+        colorRect(xPos, yPos, wd, wd)  # 正方形を描く
         if (P5.random(1) < @thr2)
-          divSquare(xPos, yPos, wd)  #正方形を分割する関数の呼び出し
+          divSquare(xPos, yPos, wd)  # 正方形を分割する関数の呼び出し
         end
         xPos += wd
       end
       wd = xEndPos - xPos
     else
       while (yPos + wd < yEndPos + 0.1) do
-        colorRect(xPos, yPos, wd, wd)  #正方形を描く
+        colorRect(xPos, yPos, wd, wd)  # 正方形を描く
         if (P5.random(1) < @thr2)
-          divSquare(xPos, yPos, wd)  #正方形を分割する関数の呼び出し
+          divSquare(xPos, yPos, wd)  # 正方形を分割する関数の呼び出し
         end
         yPos += wd
       end

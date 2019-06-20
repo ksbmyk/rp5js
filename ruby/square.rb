5057955ba137575e4f566681cd0c8f9ab796d834
Miyuki Koshiba <ksbmyk@gmail.com>
2019-06-20 22:41:48 +0900

    Add square draw base

---
+++ ruby/square.rb
@@ -0,0 +1,16 @@
+fibo = []
+def setup
+  P5.create_canvas(500, 500)
+  P5.color_mode(P5.HSB, 1)
+  draw_square()
+end
+
+def draw
+  P5.rect(5, 5, 5, 5) # TODO 描画テスト用
+end
+
+def draw_square
+end
+
+def mouse_clicked
+end

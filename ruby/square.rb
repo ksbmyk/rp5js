b128adf110aa6f5828dfb5f5f85f3f6659f2a19e
Miyuki Koshiba <ksbmyk@gmail.com>
2019-06-24 23:02:34 +0900

    Refactor next_fibo

--- ruby/square.rb
+++ ruby/square.rb
@@ -12,16 +12,14 @@ end
 def draw_square
   xPos = 0  #正方形のx位置
   yPos = 0  #正方形のy位置
-  nextFibo = next_fibo(@fibo)
-  scalar = P5.width / nextFibo # 長方形がウィンドウ幅に収まるように拡大
+  scalar = P5.width / next_fibo(@fibo) # 長方形がウィンドウ幅に収まるように拡大
   P5.background(0, 0, 1) # 描画ごとに背景を白く塗りつぶし
 
   P5.rect(5, 5, 5, 5) # TODO 描画テスト用
 end
 
 def mouse_clicked
-  nextFibo = next_fibo(@fibo)
-  @fibo = P5.append(@fibo, nextFibo)
+  @fibo = P5.append(@fibo, next_fibo(@fibo))
   draw_square
 end
 

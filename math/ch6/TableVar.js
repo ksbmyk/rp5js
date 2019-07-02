356fabf283ffa4fbe9de55ff124d169c9b700858
Miyuki Koshiba <ksbmyk@gmail.com>
2019-07-02 22:19:30 +0900

    Draw circle

--- math/ch6/TableVar.js
+++ math/ch6/TableVar.js
@@ -9,11 +9,19 @@ function setup(){
       var v = createVector(j, i);  //マスの位置 new PVectro() -> createVector()
       v.mult(scalar);
 
-      fill(255);  //マスを白くする
-      rect(v.x, v.y, scalar, scalar); //マスの描画
-      fill(0);  //数字を黒くする
-      textSize(scalar);
-      text(num, v.x, v.y + scalar);  //数字の表示
+      // fill(255);  //マスを白くする
+      // rect(v.x, v.y, scalar, scalar); //マスの描画
+      // fill(0);  //数字を黒くする
+      // textSize(scalar);
+      // text(num, v.x, v.y + scalar);  //数字の表示
+
+      // 色相に対応
+      fill(num * 1.0 / mod, 1, 1);  //数を円の色相に対応
+      noStroke();
+      ellipse(v.x, v.y, scalar / 2, scalar / 2);
+      // 円の大きさに対応
+      fill(0, 0, 0);
+      ellipse(v.x, v.y, scalar * num / mod, scalar * num / mod);  //数を円の直径に対応
     }
   }
 }

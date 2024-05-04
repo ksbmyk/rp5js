6b7a281c5aba2ecf13c6fc0caae605486e6b11f3
Miyuki Koshiba <ksbmyk@gmail.com>
2024-05-04 23:56:36 +0900

    sample

--- ruby/rubykaigi_2024.rb
+++ ruby/rubykaigi_2024.rb
@@ -14,20 +14,16 @@ def frame
   rect_size = 80
   rect_weight = 2
   P5.strokeWeight(rect_weight)
-  
-  # P5.fill(P5.color("#9cee60"))
-  # P5.rect(2, 2, 80)
-  # P5.rect(700 - 2 - 80 , 2, 80)
-  # P5.rect(2, 700 - 2 - 80, 80)
-  # P5.rect(700 - 2 - 80 , 700 - 2 - 80, 80)
 
-  P5.fill(P5.color("#9cee60"))
-  P5.rect(rect_weight, rect_weight, P5.width, rect_size)
-  P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size, P5.height)
-  P5.rect(rect_weight, P5.height - rect_weight - rect_size, P5.width, rect_size)
-  P5.rect(rect_weight, rect_weight, rect_size, P5.height)
+  # col = P5.color("#9cee60")
+  # P5.fill(col)
+  # P5.rect(rect_weight, rect_weight, P5.width, rect_size)
+  # P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size, P5.height)
+  # P5.rect(rect_weight, P5.height - rect_weight - rect_size, P5.width, rect_size)
+  # P5.rect(rect_weight, rect_weight, rect_size, P5.height)
 
-  P5.fill(P5.color("#4b92f7"))
+  col = P5.color("#4b92f7")
+  P5.fill(col)
   P5.rect(rect_weight, rect_weight, rect_size)
   P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size)
   P5.rect(rect_weight, P5.height - rect_weight - rect_size, rect_size)

502e70468f91f346e012ae55c139bd0d35fbd77f
Miyuki Koshiba <ksbmyk@gmail.com>
2024-05-04 22:26:47 +0900

    test rect

--- ruby/rubykaigi_2024.rb
+++ ruby/rubykaigi_2024.rb
@@ -18,11 +18,11 @@ def draw
   rect_weight = 2
   P5.strokeWeight(rect_weight)
   
-  P5.fill("#9cee60")
-  P5.rect(rect_weight, rect_weight, P5.width, rect_size)
-  P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size, P5.height)
-  P5.rect(rect_weight, P5.height - rect_weight - rect_size, P5.width, rect_size)
-  P5.rect(rect_weight, rect_weight, rect_size, P5.height)
+  # P5.fill("#9cee60")
+  # P5.rect(rect_weight, rect_weight, P5.width, rect_size)
+  # P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size, P5.height)
+  # P5.rect(rect_weight, P5.height - rect_weight - rect_size, P5.width, rect_size)
+  # P5.rect(rect_weight, rect_weight, rect_size, P5.height)
   
   P5.fill("#4b92f7")
   P5.rect(rect_weight, rect_weight, rect_size)

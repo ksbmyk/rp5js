442942be5992c5d1ededa4808225a637d11ebe07
Miyuki Koshiba <ksbmyk@gmail.com>
2024-05-04 23:51:07 +0900

    sample

--- ruby/rubykaigi_2024.rb
+++ ruby/rubykaigi_2024.rb
@@ -2,33 +2,34 @@ def setup
   P5.createCanvas(700, 700)
   #P5.background("#ffffff")
   # P5.noLoop
-  sea
+  frame
 end
 
 def draw
 end
-def sea
+
+def frame
   # フレーム
   P5.stroke(0)
   rect_size = 80
   rect_weight = 2
   P5.strokeWeight(rect_weight)
   
-  P5.fill("#9cee60")
-  P5.rect(2, 2, 80)
-  P5.rect(700 - 2 - 80 , 2, 80)
-  P5.rect(2, 700 - 2 - 80, 80)
-  P5.rect(700 - 2 - 80 , 700 - 2 - 80, 80)
+  # P5.fill(P5.color("#9cee60"))
+  # P5.rect(2, 2, 80)
+  # P5.rect(700 - 2 - 80 , 2, 80)
+  # P5.rect(2, 700 - 2 - 80, 80)
+  # P5.rect(700 - 2 - 80 , 700 - 2 - 80, 80)
 
-  # P5.fill("#9cee60")
-  # P5.rect(rect_weight, rect_weight, P5.width, rect_size)
-  # P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size, P5.height)
-  # P5.rect(rect_weight, P5.height - rect_weight - rect_size, P5.width, rect_size)
-  # P5.rect(rect_weight, rect_weight, rect_size, P5.height)
+  P5.fill(P5.color("#9cee60"))
+  P5.rect(rect_weight, rect_weight, P5.width, rect_size)
+  P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size, P5.height)
+  P5.rect(rect_weight, P5.height - rect_weight - rect_size, P5.width, rect_size)
+  P5.rect(rect_weight, rect_weight, rect_size, P5.height)
 
-  # P5.fill("#4b92f7")
-  # P5.rect(rect_weight, rect_weight, rect_size)
-  # P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size)
-  # P5.rect(rect_weight, P5.height - rect_weight - rect_size, rect_size)
-  # P5.rect(P5.width - rect_weight - rect_size , P5.height - rect_weight - rect_size, rect_size)
+  P5.fill(P5.color("#4b92f7"))
+  P5.rect(rect_weight, rect_weight, rect_size)
+  P5.rect(P5.width - rect_weight - rect_size , rect_weight, rect_size)
+  P5.rect(rect_weight, P5.height - rect_weight - rect_size, rect_size)
+  P5.rect(P5.width - rect_weight - rect_size , P5.height - rect_weight - rect_size, rect_size)
 end
